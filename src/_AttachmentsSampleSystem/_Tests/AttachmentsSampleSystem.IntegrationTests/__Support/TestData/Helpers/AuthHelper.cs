ae560b9e4edb710e5b09c378ae4afe0821255dd5
IAtsuta <IAtsuta@IATSUTA-NB>
2023-10-10 17:40:14 +0200

    add tests for CustomPermissions

--- src/_AttachmentsSampleSystem/_Tests/AttachmentsSampleSystem.IntegrationTests/__Support/TestData/Helpers/AuthHelper.cs
+++ src/_AttachmentsSampleSystem/_Tests/AttachmentsSampleSystem.IntegrationTests/__Support/TestData/Helpers/AuthHelper.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using Automation.ServiceEnvironment;
 
+using DocumentFormat.OpenXml.Drawing.Diagrams;
+
 using Framework.Attachments.BLL;
+using Framework.Authorization.BLL;
+using Framework.Authorization.Domain;
+using Framework.DomainDriven;
+using Framework.DomainDriven.BLL;
+using Framework.SecuritySystem;
+
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AttachmentsSampleSystem.IntegrationTests.__Support.TestData.Helpers
 {
@@ -10,6 +20,28 @@ namespace AttachmentsSampleSystem.IntegrationTests.__Support.TestData.Helpers
     {
         public AuthHelper(IServiceProvider rootServiceProvider) : base(rootServiceProvider)
         {
+        }
+
+
+        public TestBusinessRole CreateRole(string name, params SecurityOperation[] operations)
+        {
+            this.RootServiceProvider.GetRequiredService<IContextEvaluator<IAuthorizationBLLContext>>().Evaluate(DBSessionMode.Write,
+                ctx =>
+                {
+                    var role = ctx.Logics.BusinessRole.GetByNameOrCreate(name);
+
+                    foreach (var operation in operations)
+                    {
+                        new BusinessRoleOperationLink(role)
+                        {
+                                Operation = ctx.Logics.Operation.GetByName(operation.Name)
+                        };
+                    }
+
+                    ctx.Logics.BusinessRole.Save(role);
+                });
+
+            return new TestBusinessRole(name);
         }
     }
 }
